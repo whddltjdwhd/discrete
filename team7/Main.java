b909abc3aee99c5e7cc21b880eecda336f5f9af9
Rune <sungjong5k@naver.com>
2024-12-05 11:04:37 +0900

    input 파일 위치 옮김

--- team7/Main.java
+++ team7/Main.java
@@ -2,8 +2,8 @@ package team7;
 
 public class Main {
     public static void main(String[] args) {
-        Input1Reader reader1 = new Input1Reader("res/input1.txt");
-        Input2Reader reader2 = new Input2Reader("res/input2.txt");
+        Input1Reader reader1 = new Input1Reader("team7/input1.txt");
+        Input2Reader reader2 = new Input2Reader("team7/input2.txt");
 
         RunSearch dfsbfs = new DFSBFS(reader1.getList());
         RunSearch dijkstra = new Dijkstra(reader2.getList());
